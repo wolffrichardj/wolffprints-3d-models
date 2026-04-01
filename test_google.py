9efeb4137420860d1fee134a58f625ce58536044
wolffrichardj <wolffrichardj@gmail.com>
2026-04-01 11:53:35 -0500

    chore: clean up remaining python utility scripts

--- test_google.py
+++
@@ -1,9 +0,0 @@
-from googlesearch import search
-
-query = 'site:makerworld.com/en/models "mini banana buddy"'
-print("Testing google search:")
-try:
-    for j in search(query, num_results=2, advanced=True):
-        print(j.url, j.description)
-except Exception as e:
-    print(e)
